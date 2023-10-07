3aaeda41b2be1503cd83dfb2eae8aa03a50dd49f
fady jan <fady.jan95@gmail.com>
2023-10-08 00:00:33 +0300

    Css  Enhancements

--- src/components/MoTCategories/MotCategories.jsx
+++ src/components/MoTCategories/MotCategories.jsx
@@ -1,52 +1,70 @@
-import React from "react";
-import module from "./MotCategories.module.css";
+import React,{useState,useLayoutEffect} from "react";
+import "./MotCategories.css";
 import MotLogo from "../../assets/Mot.png";
 import HalfAnArc from "../../assets/HalfAnArc.png";
 import DootedArc from "../../assets/Ellipse 7.png";
 const MotCategories = () => {
+
+  const [widthState, setWidthState] = useState(window.innerWidth);
+
+
+  useLayoutEffect(() => {
+    const handleWindowResize = () => {
+      setWidthState(window.innerWidth);
+    };
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+
   return (
-    <div id={module.MotCategories}>
-      <img id={module.MoTLogo} src={MotLogo}></img>
-      <img src={HalfAnArc}></img>
-      <img id={module.DootedArc} src={DootedArc}></img>
+    <div id={"MotCategories"}>
+      <div id={widthState <680 ? 'NoLogoWrapper' : 'LogoWrapper'}>
+        <img id={"MoTLogo"} src={MotLogo}></img>
+        <img src={HalfAnArc}></img>
+        <img id={"DootedArc"} src={DootedArc}></img>
+      </div>
 
-      <div id={module.CardCategories} >
-        <div className={module.CategoryCardWrapper} style={{left:'-150px'}}>
-          <label className={module.CategoryNumber} >01</label>
-          <div className={module.CategoryDesciptionWrapper} style={{backgroundColor:'#A0C5E0'}}>
+      <div id={"CardCategories"}>
+        <div className={"CategoryCardWrapper"} style={{ left: "-150px" }}>
+          <label className={"CategoryNumber"}>01</label>
+          <div className={"CategoryDesciptionWrapper Odd"}>
             <label>Category1</label>
-            <p>abcdefalkjflkasjlkfjalskjalksjdlkasjlkdjaslkjdlkasjdlkasjdas</p>
+            <p>abcdefalkjflkasjl kfjalskjalksjdlka sjlkdjaslkjdlkasjdl kasjdas</p>
           </div>
         </div>
-        <div className={module.CategoryCardWrapper} style={{left:'-100px'}}>
-          <label className={module.CategoryNumber} >01</label>
-          <div className={module.CategoryDesciptionWrapper }style={{backgroundColor:'#193F5B',color:'white'}}>
+        <div className={"CategoryCardWrapper"} style={{ left: "-100px" }}>
+          <label className={"CategoryNumber"}>02</label>
+          <div className={"CategoryDesciptionWrapper even"}>
             <label>Category1</label>
-            <p>abcdefalkjflkasjlkfjalskjalksjdlkasjlkdjaslkjdlkasjdlkasjdas</p>
-          </div>
-        </div>
-
-        <div className={module.CategoryCardWrapper}>
-          <label className={module.CategoryNumber} >01</label>
-          <div className={module.CategoryDesciptionWrapper} style={{backgroundColor:'#A0C5E0'}}>
-            <label>Category1</label>
-            <p>abcdefalkjflkasjlkfjalskjalksjdlkasjlkdjaslkjdlkasjdlkasjdas</p>
+            <p>abcdefalkjflkas jlkfjalskjalksjdlkasj lkdjaslkjdlkasjdlk asjdas</p>
           </div>
         </div>
 
-        <div className={module.CategoryCardWrapper} style={{left:'-100px'}}>
-          <label className={module.CategoryNumber} >01</label>
-          <div className={module.CategoryDesciptionWrapper} style={{backgroundColor:'#193F5B',color:'white'}}>
+        <div className={"CategoryCardWrapper"}>
+          <label className={"CategoryNumber"}>03</label>
+          <div className={"CategoryDesciptionWrapper Odd"}>
             <label>Category1</label>
-            <p>abcdefalkjflkasjlkfjalskjalksjdlkasjlkdjaslkjdlkasjdlkasjdas</p>
+            <p>abcdefalkjflkasjl kfjalskjalksjdlka sjlkdjaslkj dlkasjdlkasjdas</p>
           </div>
         </div>
 
-        <div className={module.CategoryCardWrapper} style={{left:'-150px'}}>
-          <label className={module.CategoryNumber} >01</label>
-          <div className={module.CategoryDesciptionWrapper} style={{backgroundColor:'#A0C5E0'}}>
+        <div className={"CategoryCardWrapper"} style={{ left: "-100px" }}>
+          <label className={"CategoryNumber"}>04</label>
+          <div className={"CategoryDesciptionWrapper even"}>
             <label>Category1</label>
-            <p>abcdefalkjflkasjlkfjalskjalksjdlkasjlkdjaslkjdlkasjdlkasjdas</p>
+            <p>abcdefalkjflkasjl kfjalskjalksjdlk asjlkdjaslkjdl kasjdlkasjdas</p>
+          </div>
+        </div>
+
+        <div className={"CategoryCardWrapper"} style={{ left: "-150px" }}>
+          <label className={"CategoryNumber"}>05</label>
+          <div className="CategoryDesciptionWrapper Odd">
+            <label>Category1</label>
+            <p>abcdefalkjflkasjlkfja lskjalksjdl kasjlkdjaslkjdl kasjdlkasjdas</p>
           </div>
         </div>
       </div>
